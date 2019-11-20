0f6543bb0c76ea0698e2580d7023239e05dc12fb
Untamedy <YBolshakova@on.seo>
2019-11-20 20:28:05 +0200

    forgotpass controller

--- src/main/java/com/store/goodsstore/controllers/ForgotPassword.java
+++ src/main/java/com/store/goodsstore/controllers/ForgotPassword.java
@@ -30,7 +30,7 @@ public class ForgotPassword {
     
      @PostMapping("/forgotPass{:userEmail}")
     public ModelAndView restorePass(@PathVariable("emai") String email){
-        if(userService.(email)){
+        if(userService.existsByEmail(email)){
             return new ModelAndView("restorePassPage","email",email);
         }
         return new ModelAndView("errorPage", HttpStatus.BAD_REQUEST);
