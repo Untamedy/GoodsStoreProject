e8b0e0c9120001f40122444a8073aeb9e4cbedc8
Untamedy <Untamedy@bigmir.net>
2019-11-27 15:40:12 +0200

    storePage

---
+++ src/main/java/com/store/goodsstore/controllers/StoreController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.store.goodsstore.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Lenovo
+ */
+@Controller
+@RequestMapping("/store")
+public class StoreController {
+
+    @GetMapping()
+    public String toStorepage() {
+        return "storePage";
+
+    }
+}
