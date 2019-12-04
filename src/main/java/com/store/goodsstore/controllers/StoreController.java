76a525e0072914ebda4002324b621588b8bbc43a
Untamedy <Untamedy@bigmir.net>
2019-12-04 12:48:04 +0200

    store controller add "/gostore" controller

--- src/main/java/com/store/goodsstore/controllers/StoreController.java
+++ src/main/java/com/store/goodsstore/controllers/StoreController.java
@@ -5,10 +5,12 @@
  */
 package com.store.goodsstore.controllers;
 
+import com.store.goodsstore.dto.UserDto;
 import com.store.goodsstore.entities.Organization;
 import com.store.goodsstore.entities.Store;
 import com.store.goodsstore.entities.User;
 import com.store.goodsstore.entities.Users;
+import com.store.goodsstore.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
@@ -17,14 +19,10 @@ import org.springframework.web.servlet.ModelAndView;
 import com.store.goodsstore.services.StoreService;
 import com.store.goodsstore.services.UserService;
 import java.security.Principal;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
+import java.util.List;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -39,22 +37,28 @@ public class StoreController {
     @Autowired
     private UserService usersevice;
 
+    @Autowired
+    private OrganizationService orgService;
+
     @GetMapping("/store")
     public ModelAndView toStorepage() {
-        ModelAndView model = new ModelAndView("storePage");        
+        ModelAndView model = new ModelAndView("storePage");
         return model;
 
     }
 
     @PostMapping("/store")
-    public ModelAndView goWork() {        
+    public ModelAndView goWork() {
         return new ModelAndView("storeListPage");
     }
-    
-    @GetMapping("/allstore")
-    public ModelAndView allStore(Model model, Principal principal){
+
+    @GetMapping("/gostore")
+    public ModelAndView allStore(Model model, Principal principal) {
         String name = principal.getName();
-        return new ModelAndView("storePage","user",name);
-        
+        UserDto user = usersevice.getUsersByEmail(name);
+        Organization org = user.getOrganization();
+        Store store = org.getStore();
+        return new ModelAndView("storePage", "store", store);
+
     }
 }
