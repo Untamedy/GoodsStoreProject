ac9aec0aae6234ff0a22975de41ff31919b0b25c
Untamedy <YBolshakova@on.seo>
2020-01-13 20:13:54 +0200

    userServiceTest

--- src/test/java/serviceTests/UserServiceTest.java
+++ src/test/java/serviceTests/UserServiceTest.java
@@ -5,17 +5,23 @@
  */
 package serviceTests;
 
+import com.store.goodsstore.GoodsstoreApplication;
+import com.store.goodsstore.dto.UserDto;
 import com.store.goodsstore.entities.Users;
 import com.store.goodsstore.repository.UserRepository;
 import com.store.goodsstore.services.UserService;
+import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Before;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -23,20 +29,14 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author Lenovo
  */
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes = GoodsstoreApplication.class)
 public class UserServiceTest {
     
-    @TestConfiguration
-    static class UserServiceConfig{
     
-    @Bean
-     private UserService userSevice(){
-        return new UserService();
-    }
-}
-    
-    @Autowired
-    private UserService service;
-    
+    @Autowired 
+   UserService service;
+   
+  
     @MockBean
     private UserRepository repository;
     
@@ -44,10 +44,17 @@ public class UserServiceTest {
     public void setup(){
         Users newUser = new Users();
         newUser.setEmail("example@mail.com");
-        newUser.setName("Alex");
+        newUser.setName("User");
         
         Mockito.when(repository.findByEmail(newUser.getName())).thenReturn(newUser);
         
+    }
+    
+    @Test
+    public void getUserByEmail(){
+        String name = "User";
+        UserDto user = service.getUsersByEmail(name);
+       assertThat(user.getUsername()).isEqualTo(name);    
     }
     
     
