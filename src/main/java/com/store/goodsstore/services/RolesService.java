525bd9813df91ddfd1d00bcde82c5e48e57cfa6e
Untamedy <YBolshakova@on.seo>
2020-01-20 20:06:28 +0200

    end

--- src/main/java/com/store/goodsstore/services/RolesService.java
+++ src/main/java/com/store/goodsstore/services/RolesService.java
@@ -1,6 +1,5 @@
 package com.store.goodsstore.services;
 
-import com.store.goodsstore.init.RolesInit;
 import com.store.goodsstore.entities.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +12,7 @@ import com.store.goodsstore.repository.RolesRepository;
 @Service
 public class RolesService {
 
-    @Autowired
-    private RolesInit rolesInit;
+    
     @Autowired
     private RolesRepository repositary;
 
